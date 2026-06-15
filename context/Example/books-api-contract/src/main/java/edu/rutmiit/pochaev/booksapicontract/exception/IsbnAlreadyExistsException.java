c4fbc7e46b78ea67455499477cb80a3a785a55cf
mikhail <1028880@edu.rut-miit.ru>
2026-06-15 16:51:32 +0300

    Remove context from repository

--- context/Example/books-api-contract/src/main/java/edu/rutmiit/pochaev/booksapicontract/exception/IsbnAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package edu.rutmiit.pochaev.booksapicontract.exception;
-
-public class IsbnAlreadyExistsException extends RuntimeException {
-    public IsbnAlreadyExistsException(String isbn) {
-        super("Book with ISBN=" + isbn + " already exists");
-    }
-}
